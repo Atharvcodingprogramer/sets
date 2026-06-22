2ca8c7dfd026080b7441e7a399f072965ee8d28e
Atharvcodingprogramer <atharvsahu05@gmail.com>
2026-06-22 19:01:08 +0100

    Add files via upload

---
+++ set.py
@@ -0,0 +1,10 @@
+numbers = {1,3,5,2,6,7,9,8,4,10}
+print(numbers)
+numbers.add(11)
+print(numbers)
+numbers.remove(9)
+print(numbers)
+numbers.update((14,9))
+print(numbers)
+for i in numbers :
+    print(i)
